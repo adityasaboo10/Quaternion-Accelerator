46af648215dfac0f0e249f08ff36260f8329c4b8
RohanJain23 <rjtg2006@gmail.com>
2025-07-31 22:51:22 +0530

    Delete Full_adder.v

--- Full_adder.v
+++
@@ -1,9 +0,0 @@
-module full_adder (a,b,cin,sum,cout);
-    input a;
-    input b;
-    input cin;
-    output sum;
-    output cout;
-    assign sum= a^b^cin;
-    assign cout= a&b | a&cin | b&cin;
-endmodule
